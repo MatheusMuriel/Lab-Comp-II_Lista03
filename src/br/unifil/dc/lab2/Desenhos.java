9e9cb080dc0f6fbe41474472e4f5a1d2efe36f5c
MatheusMuriel <matheus.muriel@outlook.com>
2018-03-11 17:28:42 -0300

    Exercicios 1 e 2

--- src/br/unifil/dc/lab2/Desenhos.java
+++ src/br/unifil/dc/lab2/Desenhos.java
@@ -1,18 +1,90 @@
 package br.unifil.dc.lab2;
 
 import java.awt.*;
-import java.awt.geom.Line2D;
 
 
 /**
- * Write a description of class Desenhos here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
+ * Classe que contem as posiçoes, cores e forma dos desenhos.
+ *
+ * @author Guilherme Augusto da Rocha Manhani
+ * @author MatheusMuriel
+ * @version (1.04)
  */
 public class Desenhos
 {
+
     public static void desenhoLivre(Graphics2D pincel, Dimension dim) {
+
+    }
+    /**
+     * @param pincel variavel que receber os metodos draw para desenhar.
+     * @param Asterisco vai passar as possições do desenho.
+     * @see java.awt.Graphics, java.swing.Graphics2D
+     */
+    public static void desenharAsterisco(Graphics2D pincel, Dimension Asterisco) {
+        int x = 160;
+        int y = 200;
+        int z = (x+y)/2;
+        Graphics2D g2d = (Graphics2D) pincel.create();
+        pincel.setStroke(new BasicStroke(10));
+        pincel.drawLine(x,x,y,y);
+        pincel.drawLine(x,y,y,x);
+        pincel.drawLine(z,x,z,y);
+        pincel.drawLine(x,z,y,z);
+    }
+
+    /**
+     * @param pincel variavel que receber os metodos draw, Color e fill para desenhar.
+     * @param Smile vai passar as possições do desenho.
+     * @see java.awt.Graphics, java.swing.Graphics2D
+     */
+    public static void desenharSmile(Graphics2D pincel, Dimension Smile){
+        int x = 150;
+        int t = 30;
+
+        Graphics2D g2d =(Graphics2D) pincel.create();
+        pincel.setStroke(new BasicStroke(10));
+
+        Color verdeEscuro = new Color ( 25, 128, 82);
+        pincel.setColor(verdeEscuro);
+        pincel.fillOval(x,x,x,x);
+        pincel.setColor(Color.black);
+        pincel.drawOval(x,x,x,x);
+        pincel.fillOval( 180, 190, t,t);
+        pincel.fillOval( 240, 190, t,t);
+        pincel.fillArc(190, 210, 70, 65, 180, 180);
+        //pincel.drawLine (190,240, 260,240);
+
+        pincel.drawOval(350,x,x,x);
+        pincel.setColor(Color.YELLOW);
+        pincel.fillOval(350,x,x,x);
+        pincel.setColor(Color.black);
+        pincel.drawOval(350,x,x,x);
+        pincel.fillOval( 380, 190, t,t);
+        pincel.fillOval( 440, 190, t,t);
+        pincel.drawLine (470,250, 380,250);
+
+        pincel.drawOval(550,x,x,x);
+        pincel.setColor(Color.red);
+        pincel.fillOval(550,x,x,x);
+        pincel.setColor(Color.black);
+        pincel.drawOval(550,x,x,x);
+        pincel.fillOval( 580, 190, t,t);
+        pincel.fillOval( 640, 190, t,t);
+        pincel.drawArc(590, 235, 70, 65, 0, 180);
+        pincel.drawLine (590,175, 620,185);
+        pincel.drawLine (625,185, 660,175);
+
+    }
+
+    /**
+     * Metodo que desenha um gabinete, com 3 baias de leitor de disco, 2 espaços de disquete,
+     * entradas de fone e microfone e 2 portas USB
+     * @param pincel variavel que receber os metodos draw, Color e fill para desenhar.
+     * @param gab vai passar as possições do desenho.
+     * @see java.awt.Graphics, java.swing.Graphics2D
+     */
+    public static void desenharGabinete(Graphics2D pincel, Dimension gab){
         BasicStroke bs = new BasicStroke(1);
         BasicStroke ss = new BasicStroke(3);
 
@@ -79,16 +151,9 @@ public class Desenhos
         //Escrito da Marca
         pincel.drawString("AlunoTEC", 325,480);
 
-
     }
 
-    public static int centralizar(int valor){
-        return 0;
-
-    }
-    
-    public static void desenharAsterisco(Graphics2D g2d) {
-        throw new UnsupportedOperationException("O aluno ainda não implementou este método.");
-    }
-    
 }
+
+
+
